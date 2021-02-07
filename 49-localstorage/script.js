134d7a4588fec0b211af5707abca0e45a32f13b6
MiWChan <michelechan1996@gmail.com>
2021-02-07 00:25:12 -0300

    rafactoring localStorage

--- 49-localstorage/script.js
+++ 49-localstorage/script.js
@@ -1,34 +1,53 @@
+// Variables 
 let favorites = JSON.parse(localStorage.getItem('favorites')) || []
+const imageContainer = document.querySelector('.image')
+const button = document.querySelector('button')
 
-//pegar imagem externa 
+//events
+button.onclick = () => updateImage()
+imageContainer.onclick = () => updateAll()
+
+// Methods 
 async function getExternalImage() {
     const response = await fetch('https://source.unsplash.com/random')
-
-    document.querySelector('.image')
-    .innerHTML = `<img src ="${response.url}" />`
+    imageContainer.innerHTML = `<img src ="${response.url}" />`
 }
 getExternalImage()
 
-//clica no botao, pega imagem externa
-document.querySelector('button').onclick = function() {
-    getExternalImage()
-}
-
-//clica na imagem
-document.querySelector('.image').onclick = function() {
-    const imageContainer = document.querySelector('.image')
+function getState() {
     const imageSource = document.querySelector('.image img').src
 
     //se esta na localStorage, remover
     const index = favorites.indexOf(imageSource)
     const existsInLocalStorage = index != -1
-    if(existsInLocalStorage) {
-        favorites.splice(index, 1)
-        imageContainer.classList.remove('fav')
-    } else { //salvar no local storage
-        favorites.push(imageSource)
-        imageContainer.classList.add('fav')
-    }
+
+    return { imageSource, index, existsInLocalStorage }
+}
+
+function updateAll() {
+    updateFavorites()
+    updateClasses()
+}
+
+function updateFavorites() {
+    const { existsInLocalStorage, index, imageSource } = getState()
+
+    existsInLocalStorage ? favorites.splice(index, 1) : favorites.push(imageSource)
 
     localStorage.setItem('favorites', JSON.stringify(favorites))
+}
+
+function updateClasses(){
+    const { existsInLocalStorage } = getState()
+
+    imageContainer.classList.remove('fav')
+
+    if(existsInLocalStorage) {
+        imageContainer.classList.add('fav')
+    }
+}
+
+async function updateImage() {
+    await getExternalImage()
+    updateClasses()
 }
